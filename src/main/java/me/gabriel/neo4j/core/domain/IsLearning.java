274359a50ef6577c47fe6191e16f65019680b08b
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-21 10:31:14 -0400

    fix: adiciona identificador para o relacionamento

--- src/main/java/me/gabriel/neo4j/core/domain/IsLearning.java
+++ src/main/java/me/gabriel/neo4j/core/domain/IsLearning.java
@@ -1,6 +1,10 @@
 package me.gabriel.neo4j.core.domain;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import me.gabriel.neo4j.infra.db.repositories.NodeIdentity;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
@@ -8,9 +12,12 @@ import org.springframework.data.neo4j.core.schema.TargetNode;
  * @author daohn
  * @since 19/08/2021
  */
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @RelationshipProperties
-public class IsLearning {
+public class IsLearning extends NodeIdentity {
 
   private Long marks;
 
