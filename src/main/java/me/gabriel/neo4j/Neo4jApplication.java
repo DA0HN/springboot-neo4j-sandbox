f1f3ff6332072771fbefa6f7894a88c0a28cd5b9
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-21 09:22:25 -0400

    chore: atualiza o .gitignore 🔥

--- src/main/java/me/gabriel/neo4j/Neo4jApplication.java
+++ src/main/java/me/gabriel/neo4j/Neo4jApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan({
+//  "me.gabriel.neo4j.application.api.controllers",
+//  "me.gabriel.neo4j.application.adapters",
+//  "me.gabriel.neo4j.infra.db.repositories",
+//  "me.gabriel.neo4j.infra.db.adapters"
+//})
+//@EntityScan({
+//  "me.gabriel.neo4j.core.domain"
+//})
 public class Neo4jApplication {
 
   public static void main(String[] args) {
