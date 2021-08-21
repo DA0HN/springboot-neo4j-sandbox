2f2eb48ac51d8a273dcdb927e9fd7960c260f06a
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-21 12:13:11 -0400

    fix: corrige tipo do atributo marks

--- src/main/java/me/gabriel/neo4j/application/api/request/CreateSubjectRequest.java
+++ src/main/java/me/gabriel/neo4j/application/api/request/CreateSubjectRequest.java
@@ -4,5 +4,5 @@ package me.gabriel.neo4j.application.api.request;
  * @author daohn
  * @since 19/08/2021
  */
-public record CreateSubjectRequest(String name, String marks) {
+public record CreateSubjectRequest(String name, Long marks) {
 }
