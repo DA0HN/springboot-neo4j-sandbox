324da15ae42a4a10033bacb254ba8fee247c8fc4
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-27 22:23:22 -0400

    feat: adiciona resposta base da aplicação

--- src/main/java/me/gabriel/neo4j/application/api/response/ResponseBase.java
+++ src/main/java/me/gabriel/neo4j/application/api/response/ResponseBase.java
@@ -1,8 +1,31 @@
 package me.gabriel.neo4j.application.api.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author daohn
  * @since 19/08/2021
  */
-public class ResponseBase {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseBase<T> {
+
+  private String message;
+  private T data;
+
+  public ResponseBase<T> data(T data) {
+    this.data = data;
+    return this;
+  }
+
+  public ResponseBase<T> message(String message) {
+    this.message = message;
+    return this;
+  }
+
 }
