9eb5350f01c5e81ec0f91cbdb3d4cf9919718217
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-27 22:23:40 -0400

    feat: adiciona endpoint para criação de Student

--- src/main/java/me/gabriel/neo4j/application/api/controllers/StudentController.java
+++ src/main/java/me/gabriel/neo4j/application/api/controllers/StudentController.java
@@ -1,8 +1,38 @@
 package me.gabriel.neo4j.application.api.controllers;
 
+import lombok.AllArgsConstructor;
+import me.gabriel.neo4j.application.api.request.StudentCreateRequest;
+import me.gabriel.neo4j.application.api.response.ResponseBase;
+import me.gabriel.neo4j.application.api.response.StudentCreateResponse;
+import me.gabriel.neo4j.core.ports.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author daohn
  * @since 19/08/2021
  */
+@RestController
+@RequestMapping("/student")
+@AllArgsConstructor
 public class StudentController {
+
+  private StudentService studentService;
+
+  @PostMapping
+  public ResponseEntity<ResponseBase<StudentCreateResponse>> create(
+    @RequestBody StudentCreateRequest request
+  ) {
+    var studentCreateRequest = this.studentService.create(request);
+
+    var response = new ResponseBase<StudentCreateResponse>()
+      .data(studentCreateRequest)
+      .message("Student created successfully");
+
+    return ResponseEntity.ok(response);
+  }
+
 }
