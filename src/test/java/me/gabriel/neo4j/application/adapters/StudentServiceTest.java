0848b6b08b896166c444a9ee30d4d3746f63633d
Gabriel Honda <gabrielhondacba@gmail.com>
2021-08-29 09:36:44 -0400

    test: garante funcionamento da consulta por id do Student
    Signed-off-by: Gabriel Honda <gabrielhondacba@gmail.com>

--- src/test/java/me/gabriel/neo4j/application/adapters/StudentServiceTest.java
+++ src/test/java/me/gabriel/neo4j/application/adapters/StudentServiceTest.java
@@ -2,6 +2,7 @@ package me.gabriel.neo4j.application.adapters;
 
 import me.gabriel.neo4j.core.domain.Department;
 import me.gabriel.neo4j.core.domain.Student;
+import me.gabriel.neo4j.core.domain.StudentNotFoundException;
 import me.gabriel.neo4j.core.domain.Subject;
 import me.gabriel.neo4j.core.ports.DepartmentRepository;
 import me.gabriel.neo4j.core.ports.StudentRepository;
@@ -14,8 +15,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static me.gabriel.neo4j.utils.StudentResponseAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -24,6 +32,8 @@ import static org.mockito.Mockito.when;
  */
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
+
+  private static final long STUDENT_ID = 1L;
 
   private static final Class<Department> DEPARTMENT_REPOSITORY_ARG = Department.class;
   private static final Class<Student> STUDENT_REPOSITORY_ARG = Student.class;
@@ -47,7 +57,7 @@ class StudentServiceTest {
   }
 
   @Test
-  void shouldCreateStudentAndRelationship() {
+  void whenValidRequestShouldCreateStudentAndRelationship() {
     when(departmentRepository.create(isA(DEPARTMENT_REPOSITORY_ARG))).thenReturn(studentFactory.department());
     when(studentRepository.create(isA(STUDENT_REPOSITORY_ARG))).thenReturn(studentFactory.student());
     when(subjectRepository.create(isA(SUBJECT_REPOSITORY_ARG))).thenReturn(studentFactory.subjectList()[0]);
@@ -63,6 +73,42 @@ class StudentServiceTest {
       .hasCountry(request.country())
       .subjectsRelationshipWereCreated().containsTheseSubjects(request.subjects())
       .hasDepartment().hasSameDepartmentName(request.department());
+  }
+
+  @Test
+  void whenValidIdShouldFindStudentById() {
+
+    when(studentRepository.findById(isA(Long.class))).thenReturn(Optional.of(studentFactory.student()));
+
+    var response = service.findById(STUDENT_ID);
+
+    verify(studentRepository, times(1)).findById(isA(Long.class));
+    assertThat(response).isNotNull();
+  }
+
+  @Test
+  void whenNotFoundStudentShouldThrowException() {
+    when(studentRepository.findById(isA(Long.class))).thenReturn(Optional.empty());
+
+    assertThrows(
+      StudentNotFoundException.class,
+      () -> service.findById(STUDENT_ID)
+    );
+
+    verify(studentRepository, times(1)).findById(isA(Long.class));
+  }
+
+  @Test
+  void whenFindWithIdNullShouldThrowException() {
+
+    var exception = assertThrows(
+      IllegalArgumentException.class,
+      () -> service.findById(null)
+    );
+
+    assertEquals("Student id must be not null", exception.getMessage());
+
+    verify(studentRepository, never()).findById(isA(Long.class));
   }
 
 }
