08c9ee7f68f2669d50ce828d8e3910532025cfb6
Gabriel Honda <gabrielhondacba@gmail.com>
2021-09-26 11:15:50 -0400

    test: garante o funcionamento da consulta de todos os estudantes por parte do nome

--- src/test/java/me/gabriel/neo4j/infra/db/repositories/StudentNeo4jRepositoryTest.java
+++ src/test/java/me/gabriel/neo4j/infra/db/repositories/StudentNeo4jRepositoryTest.java
@@ -1,6 +1,8 @@
 package me.gabriel.neo4j.infra.db.repositories;
 
 import me.gabriel.neo4j.core.domain.Student;
+import me.gabriel.neo4j.utils.data.DepartmentFactory;
+import me.gabriel.neo4j.utils.data.IsLearningFactory;
 import me.gabriel.neo4j.utils.data.StudentFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,4 +35,26 @@ class StudentNeo4jRepositoryTest {
       .isNotEmpty()
       .hasSize(SIZE_STUDENT_LIST);
   }
+
+  @Test
+  void whenDatabaseHasStudentStoredShouldReturnAllStudentsUsingFilterByPartialName() {
+    final var student = new Student(
+      "gabriel",
+      "EUA",
+      1999,
+      DepartmentFactory.department(),
+      IsLearningFactory.isLearningListRandom()
+    );
+
+    this.studentNeo4jRepository.save(student);
+
+    List<Student> students = this.studentNeo4jRepository.findAllStudentsByPartialName("gab");
+
+    assertThat(students)
+      .isNotNull()
+      .isNotEmpty()
+      .hasSizeGreaterThanOrEqualTo(1);
+
+  }
+
 }
