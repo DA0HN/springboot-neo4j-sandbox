8b8e4c23fd7248f211ba793a7f844f859bf7f7a6
Gabriel Honda <gabrielhondacba@gmail.com>
2021-10-09 14:18:49 -0400

    fix: ajusta teste para a criação de um `Student` válido

--- src/test/java/me/gabriel/neo4j/application/adapters/StudentServiceTest.java
+++ src/test/java/me/gabriel/neo4j/application/adapters/StudentServiceTest.java
@@ -6,9 +6,7 @@ import me.gabriel.neo4j.core.domain.Department;
 import me.gabriel.neo4j.core.domain.IsLearning;
 import me.gabriel.neo4j.core.domain.Student;
 import me.gabriel.neo4j.core.domain.StudentNotFoundException;
-import me.gabriel.neo4j.core.domain.Subject;
 import me.gabriel.neo4j.core.ports.StudentRepository;
-import me.gabriel.neo4j.core.ports.SubjectRepository;
 import me.gabriel.neo4j.utils.data.DepartmentFactory;
 import me.gabriel.neo4j.utils.data.IsLearningFactory;
 import me.gabriel.neo4j.utils.data.StudentFactory;
@@ -30,7 +28,7 @@ import static me.gabriel.neo4j.utils.asserts.StudentResponseAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -46,15 +44,13 @@ class StudentServiceTest {
 
   private static final long STUDENT_ID = 1L;
   private static final String STUDENT_NAME = "studentName";
-  private static final Class<Department> DEPARTMENT_REPOSITORY_ARG = Department.class;
   private static final Class<Student> STUDENT_REPOSITORY_ARG = Student.class;
-  private static final Class<Subject> SUBJECT_REPOSITORY_ARG = Subject.class;
+  private static final Class<Long> LONG_ARG = Long.class;
+  private static final Class<String> STRING_ARG = String.class;
   @InjectMocks
   private StudentServiceAdapter studentService;
   @Mock
   private StudentRepository studentRepository;
-  @Mock
-  private SubjectRepository subjectRepository;
   @Mock
   private StudentRelationshipCreator<Department, String> belongsToRelationshipCreator;
   @Mock
@@ -68,11 +64,11 @@ class StudentServiceTest {
   @DisplayName("Quando os dados do estudante for válido deveria criar o `Student` e seus relacionamentos")
   @MockitoSettings(strictness = Strictness.LENIENT)
   void whenValidRequestShouldCreateStudentAndRelationship() {
-    when(this.studentRepository.create(isA(STUDENT_REPOSITORY_ARG))).thenReturn(StudentFactory.studentWithId());
-    when(this.belongsToRelationshipCreator.create(isA(String.class))).thenReturn(DepartmentFactory.departmentWithId());
-    when(this.isLearningRelationshipCreator.create(anyList())).thenReturn(IsLearningFactory.isLearningListRandom());
-
     var request = StudentFactory.createStudentRequest();
+
+    when(this.studentRepository.create(isA(STUDENT_REPOSITORY_ARG))).thenReturn(StudentFactory.studentWithId());
+    when(this.belongsToRelationshipCreator.create(isA(STRING_ARG))).thenReturn(DepartmentFactory.departmentWithId());
+    when(this.isLearningRelationshipCreator.create(eq(request.subjects()))).thenReturn(IsLearningFactory.isLearningListRandom());
 
     var response = this.studentService.create(request);
 
@@ -90,25 +86,25 @@ class StudentServiceTest {
   @DisplayName("Quando o identificador do `Student` for válido deveria retornar o `Student` com este identificador")
   void whenValidIdShouldFindStudentById() {
 
-    when(this.studentRepository.findById(isA(Long.class))).thenReturn(Optional.of(StudentFactory.studentWithId()));
+    when(this.studentRepository.findById(isA(LONG_ARG))).thenReturn(Optional.of(StudentFactory.studentWithId()));
 
     var response = this.studentService.findById(STUDENT_ID);
 
-    verify(this.studentRepository, times(1)).findById(isA(Long.class));
+    verify(this.studentRepository, times(1)).findById(isA(LONG_ARG));
     assertThat(response).isNotNull();
   }
 
   @Test
   @DisplayName("Quando o `Student` não for encontrado deveria lançar a exceção `StudentNotFoundException`")
   void whenNotFoundStudentShouldThrowException() {
-    when(this.studentRepository.findById(isA(Long.class))).thenReturn(Optional.empty());
+    when(this.studentRepository.findById(isA(LONG_ARG))).thenReturn(Optional.empty());
 
     assertThrows(
       StudentNotFoundException.class,
       () -> this.studentService.findById(STUDENT_ID)
     );
 
-    verify(this.studentRepository, times(1)).findById(isA(Long.class));
+    verify(this.studentRepository, times(1)).findById(isA(LONG_ARG));
   }
 
   @Test
@@ -122,13 +118,13 @@ class StudentServiceTest {
 
     assertEquals("Student id must be not null", exception.getMessage());
 
-    verify(this.studentRepository, never()).findById(isA(Long.class));
+    verify(this.studentRepository, never()).findById(isA(LONG_ARG));
   }
 
   @Test
   @DisplayName("Quando o nome parcial é valido deveria retornar uma lista de estudantes")
   void whenPartialNameIsValidShouldReturnStudents() {
-    when(this.studentRepository.findAllByPartialName(isA(String.class))).thenReturn(asList(
+    when(this.studentRepository.findAllByPartialName(isA(STRING_ARG))).thenReturn(asList(
       StudentFactory.studentWithId(),
       StudentFactory.studentWithId(),
       StudentFactory.studentWithId()
@@ -136,7 +132,7 @@ class StudentServiceTest {
 
     var response = this.studentService.findAllByPartialName(STUDENT_NAME);
 
-    verify(this.studentRepository, times(1)).findAllByPartialName(isA(String.class));
+    verify(this.studentRepository, times(1)).findAllByPartialName(isA(STRING_ARG));
 
     int EXPECTED_SIZE_LIST = 3;
 
